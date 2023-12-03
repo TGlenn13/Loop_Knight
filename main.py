9d6ae0e6c3cf021c07c0bead3820ef9950f272f2
TGlenn13 <tglenn@udel.edu>
2023-12-03 17:18:22 -0500

    Phase 3. Added background and monster movement.

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ from random import randint
 
 background_image("images/grass.jpg")
 
-KNIGHT_SPEED = 15
+KNIGHT_SPEED = 20
 PLAYER_HEALTH = 5
 SWORD = emoji("🔪")
-MONSTER_HEALTH = 1
 ATTACKING = False
-SKULL = emoji("💀")
 @dataclass
 class World:
     sword_timer: int
@@ -17,23 +15,28 @@ class World:
     knight: DesignerObject
     knight_speed: int
     monster: list[DesignerObject]
-    monster_health: int
-    # tier_2_monster: list[DesignerObject]
-    # boss: list[DesignerObject]
+    boss: list[DesignerObject]
     sword: DesignerObject
     weapons: list[DesignerObject]
     player_health: int
     counter: DesignerObject
     attacking: bool
-    #skull: DesignerObject
-    # weapons: list[DesignerObject]
-    # wave_number: int
 
 
 def create_world() -> World:
-    return World(0, 0, create_knight(), KNIGHT_SPEED, [], MONSTER_HEALTH, SWORD, [], PLAYER_HEALTH,
+    return World(0, 0, create_knight(), KNIGHT_SPEED, [], [], SWORD, [], PLAYER_HEALTH,
                  text("black", "Health: " + str(PLAYER_HEALTH),  40, get_width() // 2, 30), ATTACKING)
 when('starting', create_world)
+
+def filter_from(old_list: list[DesignerObject], elements_to_not_keep: list[DesignerObject]) -> list[DesignerObject]:
+    """Filters unwanted data from a list"""
+    new_values = []
+    for item in old_list:
+        if item in elements_to_not_keep:
+            destroy(item)
+        else:
+            new_values.append(item)
+    return new_values
 
 def create_knight() -> DesignerObject:
     """ Create the knight """
@@ -128,6 +131,54 @@ def update_timer(world: World):
         world.sword.y = world.knight.y
 when("updating", update_timer)
 
+def create_boss() -> DesignerObject:
+    """Creates a boss randomly on the screen"""
+    boss = emoji("👹")
+    boss.scale_x = 5
+    boss.scale_y = 5
+    boss.anchor = 'midbottom'
+    boss.x = randint(0, get_width())
+    boss.y = get_height()
+    return boss
+
+def make_boss(world: World):
+    """ Create a boss at random times"""
+    random_chance = randint(1, 75) == 50
+    if random_chance:
+        world.boss.append(create_boss())
+when("updating", make_boss)
+
+def boss_movement(world: World):
+    """Moves the boss upwards and horizontally quickly.
+    If the boss reaches the top they spawn back in
+    at the bottom"""
+    for boss in world.boss:
+        boss.x += randint(-60,60)
+        boss.y -= 1
+        if boss.y <= 0:
+            boss.y = get_height()
+when("updating", boss_movement)
+
+def boss_boundaries(world: World):
+    """keeps the boss within the screen"""
+    for boss in world.boss:
+        if boss.x > get_width():
+            boss.x = get_width() - 3
+        if boss.x <= 0:
+            boss.x = 3
+when("updating", boss_boundaries)
+
+def damage_boss(world: World):
+    """Damages boss when a weapon collides with them. Removes dead bosses"""
+    killed_bosses = []
+    for boss in world.boss:
+        if world.attacking:
+            if colliding(world.sword, boss):
+                killed_bosses.append(boss)
+                world.score += 1
+    world.boss = filter_from(world.boss, killed_bosses)
+when("updating", damage_boss)
+
 def create_monster() -> DesignerObject:
     """ Create a monster randomly on the screen """
     monster = emoji('🧟')
@@ -140,8 +191,7 @@ def create_monster() -> DesignerObject:
 
 
 def make_monster(world: World):
-    """ Create a new monster at random times, if the monster cap has not been met """
-    #not_too_many_monsters = len(world.monster) < 15
+    """ Create a new monster at random times """
     random_chance = randint(1, 30) == 15
     if random_chance:
         world.monster.append(create_monster())
@@ -167,18 +217,8 @@ def monster_boundaries(world: World):
             monster.x = 3
 when("updating", monster_boundaries)
 
-def filter_from(old_list: list[DesignerObject], elements_to_not_keep: list[DesignerObject]) -> list[DesignerObject]:
-    """Filters unwanted data from a list"""
-    new_values = []
-    for item in old_list:
-        if item in elements_to_not_keep:
-            destroy(item)
-        else:
-            new_values.append(item)
-    return new_values
-
-def damage_monster(world: World):
-    """Damages monsters when weapon collides with them. Removes dead monsters"""
+def kill_monster(world: World):
+    """Damages monsters when a weapon collides with them. Removes dead monsters"""
     killed_monsters = []
     for monster in world.monster:
         if world.attacking:
@@ -186,20 +226,14 @@ def damage_monster(world: World):
                 killed_monsters.append(monster)
                 world.score += 1
     world.monster = filter_from(world.monster, killed_monsters)
-when("updating", damage_monster)
+when("updating", kill_monster)
 
-'''
-def flash_skull(world: World):
-    timer = 10
-    for time in timer:
-        timer -= 1
-'''
 def update_health(world):
     """Updates the health displayed"""
     world.counter.text = "Health: " + str(world.player_health)
 when("updating", update_health)
 
-def damage_player(world: World):
+def monster_damages_player(world: World):
     """Damages the player when they run into a monster,
     This kills the monster too"""
     dead_monster = []
@@ -208,7 +242,18 @@ def damage_player(world: World):
             world.player_health -= 1
             dead_monster.append(monster)
             world.monster = filter_from(world.monster, dead_monster)
-when("updating", damage_player)
+when("updating", monster_damages_player)
+
+def boss_damages_player(world: World):
+    """Damages the player when they run into a monster,
+    This kills the monster too"""
+    dead_boss = []
+    for boss in world.boss:
+        if colliding(world.knight, boss):
+            world.player_health -= 1
+            dead_boss.append(boss)
+            world.boss = filter_from(world.boss, dead_boss)
+when("updating", boss_damages_player)
 
 def player_has_no_health(world: World) -> bool:
     """checks players health and returns a bool of if the player has health or not"""
@@ -218,9 +263,16 @@ def player_has_no_health(world: World) -> bool:
     return no_health
 
 def game_over(world: World):
-    world.counter.text = "GAME OVER! YOU HAVE SLAIN " + str(world.score) + " MONSTERS!"
+    """Game over screen when the player dies,
+    All monsters are wiped from the screen"""
+    if world.score == 1:
+        world.counter.text = "GAME OVER! YOU HAVE SLAIN " + str(world.score) + " MONSTER!"
+    else:
+        world.counter.text = "GAME OVER! YOU HAVE SLAIN " + str(world.score) + " MONSTERS!"
     for monster in world.monster:
         destroy(monster)
+    for boss in world.boss:
+        destroy(boss)
 when(player_has_no_health, game_over, pause)
 
 start()
